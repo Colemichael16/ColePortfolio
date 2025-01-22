4b387815cda912fbfe27c021b29453a4a675da34
Colemichael16 <colemichael999@gmail.com>
2025-01-21 23:31:06 -0700

    Added Animation to the scene Positioned the camera to view the scene on mobile

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,30 +1,28 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useGLTF, useAnimations, PerspectiveCamera } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const { scene, animations } = useGLTF("./desktop_pc/scene.glb");
+  const { actions } = useAnimations(animations, scene);
+
+  useEffect(() => {
+    if (actions) {
+      console.log(actions); // Log the actions object to the console
+      actions["Take 01"].play(); // Replace "AnimationName" with the actual name of your animation
+    }
+  }, [actions]);
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-        
-      />
       <pointLight intensity={1} />
       <primitive
-        object={computer.scene}
+        object={scene}
         scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        position={isMobile ? [-0.6, -3.5, -1] : [0.3, -3, -1.5]} //0, -3.25, -1.5
+        rotation={isMobile ? [0,1,0] : [-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
@@ -45,7 +43,7 @@ const ComputersCanvas = () => {
       setIsMobile(event.matches);
     };
 
-    // Add the callback function as a listener for changes to the media query
+    // Add the callback function as a listener to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
 
     // Remove the listener when the component is unmounted
@@ -55,23 +53,13 @@ const ComputersCanvas = () => {
   }, []);
 
   return (
-    <Canvas
-      frameloop='demand'
-      shadows
-      dpr={[1, 2]}
-      camera={{ position: [3, 3, 5], fov: 30 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
+    <Canvas>
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
+        <PerspectiveCamera makeDefault position={[1, 0, 2]} />
+        <OrbitControls enableZoom={false} target={[0, 0, 0]} />
         <Computers isMobile={isMobile} />
+        <Preload all />
       </Suspense>
-
-      <Preload all />
     </Canvas>
   );
 };
