fb5956b2e54c4b205599eb6c5627f2a8ec516ae0
Colemichael16 <colemichael999@gmail.com>
2025-03-04 14:04:58 -0700

    Removed the McLean in Hero Pteroydactyl pannel does not work with git and npm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Cole McLean</span>
+            Hi, I'm <span className='text-[#915EFF]'>Cole</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Information Science Major <br className='sm:block hidden' />
